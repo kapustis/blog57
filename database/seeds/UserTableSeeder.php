2377d86fc3c683f13fab5e1b91660f5de18f55ab
kapustis <kapustis73@gmail.com>
2020-11-11 09:42:16 +0200

    Seeds and Factories - заполнение БД тестовыми данными

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$data = [
+			[
+				'name' => 'Автор не известен',
+				'email' => 'author_unknown@mail.com',
+				'email_verified_at' => now(),
+				'password' => bcrypt('12345678'),
+			],
+			[
+				'name' => 'Автор #1',
+				'email' => 'author_1@mail.com',
+				'email_verified_at' => now(),
+				'password' => bcrypt('12345678'),
+			]
+		];
+		DB::table('users')->insert($data);
+	}
+}
